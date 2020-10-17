763cdf6374d3b8ba5573f52fc3e117e8d8650fbe
Barbara-11 <azalea.1545@gmail.com>
2020-10-17 17:47:21 -0500

    bash Practica 3

---
+++ Practica 3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function par() {
+    
+    if (num / 2 = 0); then
+        
+    echo "El numero $num es par"
+
+    else 
+        
+    echo "El numero $num es impar"
+
+    fi
+
+}
+
+num = read -p "Ingrese un numero" numero
+
+par()
