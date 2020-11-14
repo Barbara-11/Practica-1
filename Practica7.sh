5bdaa92341ffaba5e26aa4afe3e4aa4722a8d732
Barbara-11 <azalea.1545@gmail.com>
2020-11-14 13:28:39 -0600

    Script Practica 7

--- Practica7.sh
+++ Practica7.sh
@@ -3,7 +3,7 @@ ip=$(hostname -I)
 
 
 nmap --script ftp-syst $ip >Practica7.txt
-nmap --script http-jsonp-detection.nse >>Practica7.txt
+nmap --script http-jsonp-detection.nse scanme.nmap.org >>Practica7.txt
 nmap --script netbus-brute $ic >>Practica7.txt
 
 base64 < Practica7.txt > Practica7_e.txt
